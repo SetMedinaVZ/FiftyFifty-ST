f1d8ee889d903c5d7df0763831876019de6e15c1
korcline@hotmail.com <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>
2022-09-12 20:21:11 -0500

    Merge successful

--- ff/src/App.tsx
+++ ff/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import Login from "./components/Login";
+import Landing from "./components/Landing/MatrixRain";
 import {GlobalStyle} from "./GlobalStyle";
 import GlobalFonts from './fonts/fonts';
 
@@ -8,6 +9,7 @@ function App() {
   return (
       <Router>
           <Routes>
+              <Route path="/" element={<Landing/>}/>
               <Route path="/login" element={<Login/>}/>
           </Routes>
           <GlobalStyle/>
